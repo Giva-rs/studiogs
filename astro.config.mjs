8a8adb422a83dfeb4b85981648a859c537b4e875
Giva-rs <119982918+Giva-rs@users.noreply.github.com>
2026-05-22 11:06:48 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,9 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-  base: '/studiogs',
+  site: 'https://studiogs.com.br',
   compressHTML: true,
   build: {
-    inlineStylesheets: 'always',
+    inlineStylesheets: 'auto',
   },
 });
